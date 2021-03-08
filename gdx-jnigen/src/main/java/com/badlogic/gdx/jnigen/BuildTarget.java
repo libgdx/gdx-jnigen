090f1ad0749f7884644052f09fc74446a64e4433
Mario Zechner <badlogicgames@gmail.com>
2021-03-08 21:19:26 +0100

    Add macOS ARM support.
    Create a fat shared lib with an x86-64 and ARM64 slice. We could build this into separate .dylibs, but this is faster to build.

--- gdx-jnigen/src/main/java/com/badlogic/gdx/jnigen/BuildTarget.java
+++ gdx-jnigen/src/main/java/com/badlogic/gdx/jnigen/BuildTarget.java
@@ -220,9 +220,9 @@ public class BuildTarget {
 			// Mac OS X x86 & x86_64
 			BuildTarget mac = new BuildTarget(TargetOs.MacOsX, true, new String[] {"**/*.c"}, new String[0],
 				new String[] {"**/*.cpp"}, new String[0], new String[0], "",
-				"-c -Wall -O2 -arch x86_64 -DFIXED_POINT -fmessage-length=0 -fPIC -mmacosx-version-min=10.7 -stdlib=libc++",
-				"-c -Wall -O2 -arch x86_64 -DFIXED_POINT -fmessage-length=0 -fPIC -mmacosx-version-min=10.7 -stdlib=libc++",
-				"-shared -arch x86_64 -mmacosx-version-min=10.7 -stdlib=libc++");
+				"-c -Wall -O2 -arch arm64 -arch x86_64 -DFIXED_POINT -fmessage-length=0 -fPIC -mmacosx-version-min=10.7 -stdlib=libc++",
+				"-c -Wall -O2 -arch arm64 -arch x86_64 -DFIXED_POINT -fmessage-length=0 -fPIC -mmacosx-version-min=10.7 -stdlib=libc++",
+				"-shared -arch arm64 -arch x86_64 -mmacosx-version-min=10.7 -stdlib=libc++");
 			mac.cCompiler = "clang";
 			mac.cppCompiler = "clang++";
 			mac.canBuild = () -> System.getProperty("os.name").contains("Mac");
