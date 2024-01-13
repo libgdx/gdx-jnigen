43ec0a43979b94f9014c189968233bbb02d60d86
Berstanio <berstaniodev@gmail.com>
2024-01-13 11:59:47 +0100

    fix: Fix byte order issues when decomposing parameters

--- gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/ffi/ClosureInfo.java
+++ gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/ffi/ClosureInfo.java
@@ -31,23 +31,28 @@ public class ClosureInfo<T extends Closure> {
         parameter.order(ByteOrder.nativeOrder());
         for (int i = 0; i < parameters.length; i++) {
             Class<?> param = parameters[i];
-            long value = parameter.getLong();
             if (param == boolean.class) {
-                objects[i] = (value >> 7) == 1;
+                objects[i] = parameter.get() == 1;
+                parameter.position(parameter.position() + 7);
             } else if (param == char.class) {
-                objects[i] = (char) (value >> 7);
+                objects[i] = parameter.getChar();
+                parameter.position(parameter.position() + 6);
             } else if (param == byte.class) {
-                objects[i] = (byte) (value >> 7);
+                objects[i] = parameter.get();
+                parameter.position(parameter.position() + 7);
             } else if (param == short.class) {
-                objects[i] = (short) (value >> 6);
+                objects[i] = parameter.getShort();
+                parameter.position(parameter.position() + 6);
             }else if (param == int.class) {
-                objects[i] = (int) (value >> 4);
+                objects[i] = parameter.getInt();
+                parameter.position(parameter.position() + 4);
             }else if (param == long.class) {
-                objects[i] = value;
+                objects[i] = parameter.getLong();
             } else if (param == float.class) {
-                objects[i] = Float.intBitsToFloat((int)(value >> 4));
+                objects[i] = Float.intBitsToFloat(parameter.getInt());
+                parameter.position(parameter.position() + 4);
             } else if (param == double.class) {
-                objects[i] = Double.longBitsToDouble(value);
+                objects[i] = Double.longBitsToDouble(parameter.getLong());
             }
         }
         return toCall.invoke(toCallOn, objects);
