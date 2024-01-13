a4bb9b2a28a5f82a0f3c6b3bb683d8416fdcab71
Berstanio <berstaniodev@gmail.com>
2024-01-13 12:00:10 +0100

    feat: Add tests for parameter in callbacks

--- gdx-jnigen-loader/src/test/java/com/badlogic/gdx/jnigen/gc/Closures.java
+++ gdx-jnigen-loader/src/test/java/com/badlogic/gdx/jnigen/gc/Closures.java
@@ -5,12 +5,50 @@ import com.badlogic.gdx.jnigen.closure.ClosureObject;
 
 public class Closures {
 
+    /*JNI
+        #include <stdint.h>
+        #include <stdbool.h>
+     */
+
     public interface CallbackNoReturnNoArg extends Closure {
         void toCall();
     }
 
     public interface CallbackNoReturnLongArg extends Closure {
         void toCall(long arg);
+    }
+
+    public interface CallbackNoReturnIntArg extends Closure {
+        void toCall(int arg);
+    }
+
+
+    public interface CallbackNoReturnShortArg extends Closure {
+        void toCall(short arg);
+    }
+
+    public interface CallbackNoReturnByteArg extends Closure {
+        void toCall(byte arg);
+    }
+
+    public interface CallbackNoReturnCharArg extends Closure {
+        void toCall(char arg);
+    }
+
+    public interface CallbackNoReturnBooleanArg extends Closure {
+        void toCall(boolean arg);
+    }
+
+    public interface CallbackNoReturnFloatArg extends Closure {
+        void toCall(float arg);
+    }
+
+    public interface CallbackNoReturnDoubleArg extends Closure {
+        void toCall(double arg);
+    }
+
+    public interface CallbackNoReturnAllArgs extends Closure {
+        void toCall(long arg1, int arg2, short arg3, byte arg4, char arg5, boolean arg6, float arg7, double arg8);
     }
 
     public static void methodWithCallback(ClosureObject<CallbackNoReturnNoArg> closure) {
@@ -22,7 +60,54 @@ public class Closures {
     */
 
     public static native void methodWithCallbackLongArg(long fnPtr);/*
-        long long arg = 5;
-        ((void (*)(long long))fnPtr)(arg);
+        uint64_t arg = 5;
+        ((void (*)(uint64_t))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackIntArg(long fnPtr);/*
+        uint32_t arg = 5;
+        ((void (*)(uint32_t))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackShortArg(long fnPtr);/*
+        uint16_t arg = 5;
+        ((void (*)(uint16_t))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackByteArg(long fnPtr);/*
+        uint8_t arg = 5;
+        ((void (*)(uint8_t))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackCharArg(long fnPtr);/*
+        uint16_t arg = 5;
+        ((void (*)(uint16_t))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackBooleanArg(long fnPtr);/*
+        bool arg = true;
+        ((void (*)(bool))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackFloatArg(long fnPtr);/*
+        float arg = 5.5;
+        ((void (*)(float))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackDoubleArg(long fnPtr);/*
+        double arg = 5.5;
+        ((void (*)(double))fnPtr)(arg);
+    */
+
+    public static native void methodWithCallbackAllArgs(long fnPtr);/*
+        uint64_t arg1 = 1;
+        uint32_t arg2 = 2;
+        uint16_t arg3 = 3;
+        uint8_t arg4 = 4;
+        uint16_t arg5 = 5;
+        bool arg6 = true;
+        float arg7 = 6.6;
+        double arg8 = 7.7;
+        ((void (*)(uint64_t, uint32_t, uint16_t, uint8_t, uint16_t, bool, float, double))fnPtr)(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
     */
 }
