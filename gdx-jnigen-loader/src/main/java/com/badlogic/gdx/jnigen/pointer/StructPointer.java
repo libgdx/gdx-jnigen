93191f8ce5bc8fe2743cdeeb073e2324edba7452
Berstanio <berstaniodev@gmail.com>
2024-01-22 16:31:18 +0100

    feat: Rework StructPointer as abstract class

--- gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/StructPointer.java
+++ gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/StructPointer.java
@@ -2,27 +2,36 @@ package com.badlogic.gdx.jnigen.pointer;
 
 import com.badlogic.gdx.jnigen.Global;
 
-public interface StructPointer<T extends Struct> {
+public abstract class StructPointer<T extends Struct> extends Pointing {
+
+    protected StructPointer(long pointer, boolean freeOnGC) {
+        super(pointer, freeOnGC);
+    }
+
+    public StructPointer(long size) {
+        super(size);
+    }
 
     public static <T extends Struct> StructPointer<T> of(Class<T> of) {
         // Make better, by being able to reference the default constructir
         return Global.getStructPointer(of).create(Global.malloc(Global.getStructSize(of)), true);
     }
 
-    default T get() {
+    public T get() {
         long newPtr = Global.clone(getPointer(), getSize());
         return Global.getPointingSupplier(getStructClass()).create(newPtr, true);
     }
 
-    default void set(T struct) {
+    public T asStruct() {
+        // Shiiit tooooo, we somehow need to prevent freeing to early
+        return Global.getPointingSupplier(getStructClass()).create(getPointer(), false);
+    }
+
+    public void set(T struct) {
         Global.memcpy(getPointer(), struct.getPointer(), getSize());
     }
 
-    Class<T> getStructClass();
+    public abstract Class<T> getStructClass();
 
-    long getSize();
-
-    long getPointer();
-
-    void free();
+    public abstract long getSize();
 }
