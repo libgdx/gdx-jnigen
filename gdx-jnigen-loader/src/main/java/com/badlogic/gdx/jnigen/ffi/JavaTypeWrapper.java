d341b22eb5f632336b8d66719f0b5c6b2799f078
Berstanio <berstaniodev@gmail.com>
2024-01-23 13:43:35 +0100

    feat: Reduce Closure primitive garbage creation and support multi-thread invoke

---
+++ gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/ffi/JavaTypeWrapper.java
@@ -0,0 +1,80 @@
+package com.badlogic.gdx.jnigen.ffi;
+
+import com.badlogic.gdx.jnigen.pointer.Pointing;
+
+public final class JavaTypeWrapper {
+
+    private final Class<?> wrappingClass;
+    private long wrappingType;
+    private Pointing wrappingPointing;
+
+    public JavaTypeWrapper(Class<?> wrappingClass) {
+        this.wrappingClass = wrappingClass;
+    }
+
+    public void setValue(long value) {
+        this.wrappingType = value;
+    }
+
+    public void setValue(Pointing wrappingPointing) {
+        this.wrappingPointing = wrappingPointing;
+    }
+
+    public Class<?> getWrappingClass() {
+        return wrappingClass;
+    }
+
+    public boolean asBoolean() {
+        if (wrappingClass != boolean.class)
+            throw new IllegalArgumentException();
+        return wrappingType != 0;
+    }
+
+    public byte asByte() {
+        if (wrappingClass != byte.class)
+            throw new IllegalArgumentException();
+        return (byte)wrappingType;
+    }
+
+    public char asChar() {
+        if (wrappingClass != char.class)
+            throw new IllegalArgumentException();
+        return (char)wrappingType;
+    }
+
+    public short asShort() {
+        if (wrappingClass != short.class)
+            throw new IllegalArgumentException();
+        return (short)wrappingType;
+    }
+
+    public int asInt() {
+        if (wrappingClass != int.class)
+            throw new IllegalArgumentException();
+        return (int)wrappingType;
+    }
+
+    public long asLong() {
+        if (wrappingClass != long.class)
+            throw new IllegalArgumentException();
+        return wrappingType;
+    }
+
+    public float asFloat() {
+        if (wrappingClass != float.class)
+            throw new IllegalArgumentException();
+        return Float.intBitsToFloat((int)wrappingType);
+    }
+
+    public double asDouble() {
+        if (wrappingClass != double.class)
+            throw new IllegalArgumentException();
+        return Double.longBitsToDouble(wrappingType);
+    }
+
+    public Pointing asPointing() {
+        if (!Pointing.class.isAssignableFrom(wrappingClass))
+            throw new IllegalArgumentException();
+        return wrappingPointing;
+    }
+}
