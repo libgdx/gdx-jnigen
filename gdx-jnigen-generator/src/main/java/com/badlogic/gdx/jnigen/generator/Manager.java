8a11c7cf41a4493140f802b78b0460f2991c8e1c
Berstanio <berstaniodev@gmail.com>
2024-01-29 18:03:19 +0100

    feat: WIP add generator

--- gdx-jnigen-generator/src/main/java/com/badlogic/gdx/jnigen/generator/Manager.java
+++ gdx-jnigen-generator/src/main/java/com/badlogic/gdx/jnigen/generator/Manager.java
@@ -3,7 +3,12 @@ package com.badlogic.gdx.jnigen.generator;
 import com.badlogic.gdx.jnigen.generator.types.Emitable;
 import com.badlogic.gdx.jnigen.generator.types.StructType;
 import com.badlogic.gdx.jnigen.generator.types.TypeKind;
+import com.github.javaparser.ast.CompilationUnit;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -34,6 +39,17 @@ public class Manager {
     }
 
 
+    public void emit() {
+        structs.values().forEach(structType -> {
+            CompilationUnit cu = new CompilationUnit("test");
+            structType.write(cu);
+            try {
+                Files.write(Paths.get("gdx-jnigen-generator/src/test/java/test"), cu.toString().getBytes(StandardCharsets.UTF_8));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
     public static Manager getInstance() {
         return instance;
