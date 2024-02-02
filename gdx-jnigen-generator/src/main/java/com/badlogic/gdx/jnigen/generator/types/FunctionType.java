a3168088842b1046b58eb700979998a9c296923a
Berstanio <berstaniodev@gmail.com>
2024-02-02 23:49:50 +0100

    feat: Initial implementation of emitting function bindings

--- gdx-jnigen-generator/src/main/java/com/badlogic/gdx/jnigen/generator/types/FunctionType.java
+++ gdx-jnigen-generator/src/main/java/com/badlogic/gdx/jnigen/generator/types/FunctionType.java
@@ -3,8 +3,11 @@ package com.badlogic.gdx.jnigen.generator.types;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.ReturnStmt;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class FunctionType {
 
@@ -18,33 +21,45 @@ public class FunctionType {
         this.returnType = returnType;
     }
 
-    public void write(ClassOrInterfaceDeclaration wrappingClass) {
-        boolean doWeNeedToUnwrap = !returnType.getTypeKind().isPrimitive() || Arrays.stream(arguments).anyMatch(type -> !type.getDefinition().getTypeKind().isPrimitive());
-
+    public void write(ClassOrInterfaceDeclaration wrappingClass, HashMap<MethodDeclaration, String> patchNativeMethod) {
         MethodDeclaration callMethod = wrappingClass.addMethod(name);
 
+
+
+        MethodDeclaration nativeMethod = wrappingClass.addMethod(name + "_internal").setPrivate(true).setNative(true).setBody(null);
+        callMethod.setType(returnType.resolveJavaClass());
+        if (returnType.getTypeKind().isPrimitive())
+            nativeMethod.setType(returnType.resolveJavaClass());
+        else
+            nativeMethod.setType(long.class);
+        MethodCallExpr callExpr = new MethodCallExpr(nativeMethod.getNameAsString());
+
+        StringBuilder nativeBody = new StringBuilder();
+        nativeBody.append(name).append("(");
         for (NamedType namedType : arguments) {
             callMethod.addParameter(namedType.getDefinition().resolveJavaClass(), namedType.getName());
-        }
-        callMethod.setType(returnType.resolveJavaClass());
-        if (!doWeNeedToUnwrap) {
-            callMethod.setBody(null);
-            callMethod.setNative(true);
-        } else {
-            MethodDeclaration nativeMethod = wrappingClass.addMethod(name + "_internal").setPrivate(true).setNative(true);
-            MethodCallExpr callExpr = new MethodCallExpr(nativeMethod.getNameAsString());
-            for (NamedType namedType : arguments) {
-                if (namedType.getDefinition().getTypeKind().isPrimitive()) {
-                    callMethod.addParameter(namedType.getDefinition().resolveJavaClass(), namedType.getName());
-                    callExpr.addArgument(namedType.getName());
-                } else {
-                    callMethod.addParameter(long.class, namedType.getName());
-                    callExpr.addArgument(namedType.getName() + ".getPointer()");
-                }
+            nativeBody.append(namedType.getName()).append(", ");
+            if (namedType.getDefinition().getTypeKind().isPrimitive()) {
+                nativeMethod.addParameter(namedType.getDefinition().resolveJavaClass(), namedType.getName());
+                callExpr.addArgument(namedType.getName());
+            } else {
+                nativeMethod.addParameter(long.class, namedType.getName());
+                callExpr.addArgument(namedType.getName() + ".getPointer()");
             }
-            if (returnType.getTypeKind() != TypeKind.VOID) {
+        }
+        if (arguments.length != 0)
+            nativeBody.delete(nativeBody.length() - 2, nativeBody.length());
+        nativeBody.append(");");
 
-            }
+        BlockStmt body = new BlockStmt();
+        if (returnType.getTypeKind() != TypeKind.VOID) {
+            nativeBody.insert(0, "return ");
+            body.addStatement(new ReturnStmt(callExpr));
+        } else {
+            body.addStatement(callExpr);
         }
+        callMethod.setBody(body);
+
+        patchNativeMethod.put(nativeMethod, nativeBody.toString());
     }
 }
--- gdx-jnigen-generator/src/main/java/com/badlogic/gdx/jnigen/generator/types/GlobalType.java
+++ gdx-jnigen-generator/src/main/java/com/badlogic/gdx/jnigen/generator/types/GlobalType.java
@@ -4,22 +4,34 @@ import com.badlogic.gdx.jnigen.pointer.CType;
 import com.badlogic.gdx.jnigen.pointer.Signed;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class GlobalType {
 
     private final List<ClosureType> closures = new ArrayList<>();
+    private final List<FunctionType> functions = new ArrayList<>();
 
     public void addClosure(ClosureType closureType) {
         closures.add(closureType);
     }
 
-    public void write(CompilationUnit cu) {
+    public void addFunction(FunctionType functionType) {
+        functions.add(functionType);
+    }
+
+
+    public void write(CompilationUnit cu, HashMap<MethodDeclaration, String> patchNativeMethods) {
         cu.addImport(CType.class);
         cu.addImport(Signed.class);
         ClassOrInterfaceDeclaration global = cu.addClass("Global");
+
+        for (FunctionType functionType : functions) {
+            functionType.write(global, patchNativeMethods);
+        }
 
         for (ClosureType closureType : closures) {
             closureType.write(global);
