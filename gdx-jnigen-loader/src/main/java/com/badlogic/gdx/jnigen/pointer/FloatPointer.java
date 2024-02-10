0ede287d168a48516c2eed163f07351ee390191c
Berstanio <berstaniodev@gmail.com>
2024-02-10 11:34:45 +0100

    feat(loader): Implement first level of pointer (PtrPtr missing)

---
+++ gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/FloatPointer.java
@@ -0,0 +1,24 @@
+package com.badlogic.gdx.jnigen.pointer;
+
+import com.badlogic.gdx.jnigen.CHandler;
+
+public class FloatPointer extends Pointing {
+
+    private static final int __float_size = 4;
+
+    public FloatPointer(long pointer, boolean freeOnGC) {
+        super(pointer, freeOnGC);
+    }
+
+    public FloatPointer(long size) {
+        super(__float_size * size);
+    }
+
+    public float getFloat(int index) {
+        return Float.intBitsToFloat((int)CHandler.getPointerPart(getPointer(), __float_size, index * __float_size));
+    }
+
+    public void setFloat(float value, int index) {
+        CHandler.setPointerPart(getPointer(), __float_size, index * __float_size, Float.floatToIntBits(value));
+    }
+}
