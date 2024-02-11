e4aee3e6c1c3549ae36a61fb5703333acffc6f01
Berstanio <berstaniodev@gmail.com>
2024-02-11 12:09:30 +0100

    feat(generator): Add tests for passing pointer

---
+++ gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/tests/PointerTest.java
@@ -0,0 +1,19 @@
+package com.badlogic.jnigen.tests;
+
+import com.badlogic.gdx.jnigen.pointer.CSizedIntPointer;
+import com.badlogic.jnigen.generated.TestData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PointerTest extends BaseTest {
+
+    @Test
+    public void testPassPointer() {
+        CSizedIntPointer pointer = new CSizedIntPointer("int", 1);
+        for (int i = -10; i < 10; i++) {
+            pointer.setInt(i, 0);
+            assertEquals(i, TestData.passIntPointer(pointer));
+        }
+    }
+}
