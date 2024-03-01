9709bfd57d6513c95d8cfada47dde21bef7a7434
Berstanio <berstaniodev@gmail.com>
2024-03-01 10:35:44 +0100

    feat: Restructure Struct layout to allow for better union support

--- gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/Struct.java
+++ gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/Struct.java
@@ -1,18 +1,14 @@
 package com.badlogic.gdx.jnigen.pointer;
 
-public abstract class Struct extends Pointing {
+public abstract class Struct extends StackElement {
 
     protected Struct(long pointer, boolean freeOnGC) {
         super(pointer, freeOnGC);
     }
 
     protected Struct(int size) {
-        super(size, true, true);
+        super(size);
     }
-
-    public abstract StructPointer<?> asPointer();
-
-    public abstract long getSize();
 
     public abstract long getFFIType();
 }
