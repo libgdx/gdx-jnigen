7f91abf61d12cb85ea74adfea55d5f0453796b60
Berstanio <berstaniodev@gmail.com>
2024-03-19 23:05:13 +0100

    feat(generator): Observer anonymous closures

--- gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/generated/structs/AnonymousStructFieldArray.java
+++ gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/generated/structs/AnonymousStructFieldArray.java
@@ -13,7 +13,7 @@ public final class AnonymousStructFieldArray extends Struct {
     private final static long __ffi_type;
 
     static {
-        __ffi_type = FFITypes.getCTypeInfo(11).getFfiType();
+        __ffi_type = FFITypes.getCTypeInfo(12).getFfiType();
         __size = CHandler.getSizeFromFFIType(__ffi_type);
     }
 
