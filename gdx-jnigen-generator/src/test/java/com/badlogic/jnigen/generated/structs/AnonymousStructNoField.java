94c2b1c8ca26ee00969f30b75c2e996ec847a689
Berstanio <berstaniodev@gmail.com>
2024-03-20 11:36:40 +0100

    chore(generator): Update test bindings

--- gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/generated/structs/AnonymousStructNoField.java
+++ gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/generated/structs/AnonymousStructNoField.java
@@ -12,7 +12,7 @@ public final class AnonymousStructNoField extends Struct {
     private final static long __ffi_type;
 
     static {
-        __ffi_type = FFITypes.getCTypeInfo(13).getFfiType();
+        __ffi_type = FFITypes.getCTypeInfo(17).getFfiType();
         __size = CHandler.getSizeFromFFIType(__ffi_type);
     }
 
