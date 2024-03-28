a174200952b79545d4be009907e306dfc86fa97c
Berstanio <berstaniodev@gmail.com>
2024-03-28 12:58:27 +0100

    feat: Move EnumPointer to subclass

--- gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/generated/enums/TestEnum.java
+++ gdx-jnigen-generator/src/test/java/com/badlogic/jnigen/generated/enums/TestEnum.java
@@ -1,5 +1,6 @@
 package com.badlogic.jnigen.generated.enums;
 
+import com.badlogic.gdx.jnigen.pointer.EnumPointer;
 import com.badlogic.gdx.jnigen.c.CEnum;
 
 public enum TestEnum implements CEnum {
@@ -21,4 +22,28 @@ public enum TestEnum implements CEnum {
     }
 
     private final static TestEnum[] _values = { FIRST, SECOND, null, null, THIRD };
+
+    public static final class TestEnumPointer extends EnumPointer<TestEnum> {
+
+        public TestEnumPointer(long pointer, boolean freeOnGC) {
+            super(pointer, freeOnGC);
+        }
+
+        public TestEnumPointer() {
+            this(1, true, true);
+        }
+
+        public TestEnumPointer(int count, boolean freeOnGC, boolean guard) {
+            super(count, freeOnGC, guard);
+        }
+
+        public TestEnum.TestEnumPointer guardCount(long count) {
+            super.guardCount(count);
+            return this;
+        }
+
+        protected TestEnum getEnum(int index) {
+            return getByIndex(index);
+        }
+    }
 }
--- gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/IntToCEnumFunction.java
+++
@@ -1,9 +0,0 @@
-package com.badlogic.gdx.jnigen.pointer;
-
-import com.badlogic.gdx.jnigen.c.CEnum;
-
-@FunctionalInterface
-public interface IntToCEnumFunction<T extends CEnum> {
-
-    T apply(int id);
-}
