ad01d87368e003fa508a7277bd2a190306c6b94e
Berstanio <berstaniodev@gmail.com>
2024-03-28 14:12:38 +0100

    feat: Enable strict CSizedIntPointer PointerPointer verification

--- gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/PointerPointer.java
+++ gdx-jnigen-loader/src/main/java/com/badlogic/gdx/jnigen/pointer/PointerPointer.java
@@ -1,28 +1,30 @@
 package com.badlogic.gdx.jnigen.pointer;
 
 import com.badlogic.gdx.jnigen.CHandler;
+import com.badlogic.gdx.jnigen.c.CTypeInfo;
 
 public class PointerPointer<T extends Pointing> extends Pointing {
 
     private static final int __pointer_size = CHandler.POINTER_SIZE;
-    private final PointerDereferenceSupplier<?> supplier;
+    private final PointerDereferenceSupplier<T> supplier;
+    private CTypeInfo cTypeInfo;
     private final int depth;
 
-    public PointerPointer(long pointer, boolean freeOnGC, PointerDereferenceSupplier<?> supplier, int depth) {
+    public PointerPointer(long pointer, boolean freeOnGC, PointerDereferenceSupplier<T> supplier, int depth) {
         super(pointer, freeOnGC);
         this.supplier = supplier;
         this.depth = depth;
     }
 
-    public PointerPointer(PointerDereferenceSupplier<?> supplier, int depth) {
+    public PointerPointer(PointerDereferenceSupplier<T> supplier, int depth) {
         this(1, supplier, depth);
     }
 
-    public PointerPointer(int size, PointerDereferenceSupplier<?> supplier, int depth) {
+    public PointerPointer(int size, PointerDereferenceSupplier<T> supplier, int depth) {
         this(size, true, true, supplier, depth);
     }
 
-    public PointerPointer(int size, boolean freeOnGC, boolean guard, PointerDereferenceSupplier<?> supplier, int depth) {
+    public PointerPointer(int size, boolean freeOnGC, boolean guard, PointerDereferenceSupplier<T> supplier, int depth) {
         super(size, freeOnGC, guard);
         this.supplier = supplier;
         this.depth = depth;
@@ -40,6 +42,19 @@ public class PointerPointer<T extends Pointing> extends Pointing {
             throw new IllegalArgumentException("Expected depth " + depth + " but got " + this.depth);
     }
 
+    public PointerPointer<T> setBackingCType(String name) {
+        this.cTypeInfo = CHandler.getCTypeInfo(name);
+        return this;
+    }
+
+    public void assertCTypeBackingAndDepth(String name, int depth) {
+        assertDepth(depth);
+        if (cTypeInfo == null)
+            throw new IllegalArgumentException("PointerPointer has no recorded CTypeInfo, set it with setBackingCType");
+        if (!cTypeInfo.getName().equals(name))
+            throw new IllegalArgumentException("Expected type " + name + " does not match actual type " + cTypeInfo.getName());
+    }
+
     public int getDepth() {
         return depth;
     }
@@ -48,14 +63,28 @@ public class PointerPointer<T extends Pointing> extends Pointing {
         return getValue(0);
     }
 
-    @SuppressWarnings("unchecked")
+    private void assertChild(T pointerObj) {
+        if (depth > 2 && !(pointerObj instanceof PointerPointer))
+            throw new IllegalArgumentException("Dereference of depth " + depth + " > 2 should result in an PointerPointer");
+        if (pointerObj instanceof PointerPointer && ((PointerPointer<?>)pointerObj).getDepth() != depth - 1)
+            throw new IllegalArgumentException("Depth hasn't decreased through dereferencing of PointerPointer");
+        if (depth == 2 && cTypeInfo != null && !(pointerObj instanceof CSizedIntPointer))
+            throw new IllegalArgumentException("PointerPointer has set a backing CTypeInfo, but doesn't dereference to an CSizedIntPointer");
+        if (pointerObj instanceof CSizedIntPointer) {
+            if (cTypeInfo == null)
+                throw new IllegalArgumentException("PointerPointer dereferences to CTypeInfo, but has not CType info, set it with setBackingCType");
+            CSizedIntPointer intPointer = (CSizedIntPointer) pointerObj;
+            intPointer.assertHasCTypeBacking(cTypeInfo.getName());
+        }
+    }
+
     public T getValue(int index) {
         int offset = index * __pointer_size;
         assertBounds(offset);
         long pointer = CHandler.getPointerPart(getPointer(), __pointer_size, offset);
-        if (depth == 2)
-            return (T)supplier.create(pointer, false);
-        return (T)new PointerPointer<>(pointer, false, supplier, depth - 1);
+        T pointerObj = supplier.create(pointer, false);
+        assertChild(pointerObj);
+        return pointerObj;
     }
 
     public void setValue(T value) {
@@ -65,6 +94,7 @@ public class PointerPointer<T extends Pointing> extends Pointing {
     public void setValue(T value, int index) {
         int offset = index * __pointer_size;
         assertBounds(offset);
+        assertChild(value);
         CHandler.setPointerPart(getPointer(), __pointer_size, offset, value.getPointer());
     }
 }
