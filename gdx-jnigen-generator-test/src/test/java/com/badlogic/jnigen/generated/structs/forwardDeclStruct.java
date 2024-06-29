c640de8a1822c06992e684254231c5099a429f33
Berstanio <berstaniodev@gmail.com>
2024-06-29 23:32:47 +0200

    --

---
+++ gdx-jnigen-generator-test/src/test/java/com/badlogic/jnigen/generated/structs/forwardDeclStruct.java
@@ -0,0 +1,66 @@
+package com.badlogic.jnigen.generated.structs;
+
+import com.badlogic.gdx.jnigen.runtime.CHandler;
+import com.badlogic.gdx.jnigen.runtime.pointer.Struct;
+import com.badlogic.gdx.jnigen.runtime.pointer.StackElementPointer;
+import com.badlogic.jnigen.generated.FFITypes;
+
+public final class forwardDeclStruct extends com.badlogic.gdx.jnigen.runtime.pointer.Struct {
+
+    private final static int __size;
+
+    private final static long __ffi_type;
+
+    static {
+        __ffi_type = FFITypes.getCTypeInfo(21).getFfiType();
+        __size = CHandler.getSizeFromFFIType(__ffi_type);
+    }
+
+    public forwardDeclStruct(long pointer, boolean freeOnGC) {
+        super(pointer, freeOnGC);
+    }
+
+    public forwardDeclStruct() {
+        super(__size);
+    }
+
+    public long getSize() {
+        return __size;
+    }
+
+    public long getFFIType() {
+        return __ffi_type;
+    }
+
+    public forwardDeclStruct.forwardDeclStructPointer asPointer() {
+        return new forwardDeclStruct.forwardDeclStructPointer(getPointer(), getsGCFreed());
+    }
+
+    public static final class forwardDeclStructPointer extends StackElementPointer<forwardDeclStruct> {
+
+        public forwardDeclStructPointer(long pointer, boolean freeOnGC) {
+            super(pointer, freeOnGC);
+        }
+
+        public forwardDeclStructPointer() {
+            this(1, true, true);
+        }
+
+        public forwardDeclStructPointer(int count, boolean freeOnGC, boolean guard) {
+            super(__size, count, freeOnGC, guard);
+        }
+
+        public forwardDeclStruct.forwardDeclStructPointer guardCount(long count) {
+            super.guardCount(count);
+            return this;
+        }
+
+        public int getSize() {
+            return __size;
+        }
+
+        protected forwardDeclStruct createStackElement(long ptr, boolean freeOnGC) {
+            return new forwardDeclStruct(ptr, freeOnGC);
+        }
+    }
+}
