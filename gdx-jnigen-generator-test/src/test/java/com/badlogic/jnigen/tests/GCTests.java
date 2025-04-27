be79afb4fb2aea499a9f28332a9a636350798e6e
Berstanio <berstaniodev@gmail.com>
2025-04-27 23:19:34 +0200

    --

--- gdx-jnigen-generator-test/src/test/java/com/badlogic/jnigen/tests/GCTests.java
+++ gdx-jnigen-generator-test/src/test/java/com/badlogic/jnigen/tests/GCTests.java
@@ -2,6 +2,7 @@ package com.badlogic.jnigen.tests;
 
 import com.badlogic.gdx.jnigen.runtime.gc.GCHandler;
 import com.badlogic.gdx.jnigen.runtime.pointer.Pointing;
+import com.badlogic.gdx.jnigen.runtime.pointer.VoidPointer;
 import org.junit.jupiter.api.Test;
 
 import java.lang.ref.WeakReference;
@@ -14,7 +15,7 @@ public class GCTests extends BaseTest {
     @Test
     public void testPointingReleased() {
         assertEquals(0, GCHandler.nativeObjectCount());
-        Pointing pointing = new Pointing(0, true);
+        Pointing pointing = new VoidPointer(0, true);
         assertEquals(1, GCHandler.nativeObjectCount());
         WeakReference<Pointing> toCheckGC = new WeakReference<>(pointing);
         pointing = null;
@@ -28,8 +29,8 @@ public class GCTests extends BaseTest {
     @Test
     public void testPointingNoDoubleReleased() {
         assertEquals(0, GCHandler.nativeObjectCount());
-        Pointing pointing = new Pointing(0, true);
-        Pointing samePointer = new Pointing(pointing.getPointer(), false);
+        Pointing pointing = new VoidPointer(0, true);
+        Pointing samePointer = new VoidPointer(pointing.getPointer(), false);
         samePointer.setParent(pointing);
         assertEquals(pointing.getPointer(), samePointer.getPointer());
         assertEquals(1, GCHandler.nativeObjectCount());
@@ -49,8 +50,8 @@ public class GCTests extends BaseTest {
     @Test
     public void testPointingFreeEnqueuedPointer() {
         assertEquals(0, GCHandler.nativeObjectCount());
-        Pointing pointing = new Pointing(1, true);
-        Pointing manually = new Pointing(pointing.getPointer(), false);
+        Pointing pointing = new VoidPointer(1, true);
+        Pointing manually = new VoidPointer(pointing.getPointer(), false);
         manually.setParent(pointing);
         assertThrows(IllegalStateException.class, manually::free);
     }
